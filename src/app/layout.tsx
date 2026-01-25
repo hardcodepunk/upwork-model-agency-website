7be0d0f58846dcd1d0134e3d41eb1f6a8e0ed71c
Hardcodepunk <howl@hardcodepunk.be>
2026-01-26 00:47:20 +0100

    fix: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,15 @@ import "@/app/globals.css"
 export const metadata: Metadata = {
   title: "Valhalla Girls",
   description: "Become a Valhalla Girl and enter today.",
+  icons: {
+    icon: [{ url: "/favicon.ico" }, { url: "/icon.svg", type: "image/svg+xml" }],
+    apple: "/apple-icon.png",
+  },
+  appleWebApp: {
+    title: "Valhalla Girls",
+    statusBarStyle: "default",
+    capable: true,
+  },
 }
 
 const inter = Inter({
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,16 +7,16 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Become a Valhalla Girl and enter today.",
     start_url: "/",
     display: "standalone",
-    background_color: "#fff",
+    background_color: "#ffffff",
     theme_color: "#94754A",
     icons: [
       {
-        src: "/images/icon-192.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/images/icon-512.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
